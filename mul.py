0f843ca694ec36a458bca8b66c24a54fed2e669a
Kruti Rathod <121673218+smiling29329@users.noreply.github.com>
2023-10-04 11:20:13 +0530

    Add files via upload

---
+++ mul.py
@@ -0,0 +1,10 @@
+def multiply_numbers(a, b):
+    product = a * b
+    return product
+
+# Example Usage:
+num1 = 10
+num2 = 5
+
+result = multiply_numbers(num1, num2)
+print(f"The product of {num1} and {num2} is: {result}")
