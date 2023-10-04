8c85b6c25c98176dbadd4a427b56db538a7ec55c
Kruti Rathod <121673218+smiling29329@users.noreply.github.com>
2023-10-04 11:37:56 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+def add_numbers(a, b):
+    sum_result = a + b
+    return sum_result
+
+# Example Usage:
+num1 = 10
+num2 = 5
+
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
