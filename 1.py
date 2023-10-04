fb03428215a47b3ddb1f715ccbb6542253582938
Kruti Rathod <121673218+smiling29329@users.noreply.github.com>
2023-10-04 11:24:00 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,8 +3,8 @@ def add_numbers(a, b):
     return sum_result
 
 # Example Usage:
-num1 = 10
-num2 = 5
+num1 = 25
+num2 = 15
 
 result = add_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is: {result}")
