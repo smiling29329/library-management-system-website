99936545e57b7fc80c81b6ecba5f734d4682ed9e
Kruti Rathod <121673218+smiling29329@users.noreply.github.com>
2023-10-04 11:42:32 +0530

    Update mul.py

--- mul.py
+++ mul.py
@@ -3,8 +3,8 @@ def multiply_numbers(a, b):
     return product
 
 # Example Usage:
-num1 = 10
-num2 = 5
+num1 = 50
+num2 = 15
 
 result = multiply_numbers(num1, num2)
 print(f"The product of {num1} and {num2} is: {result}")
